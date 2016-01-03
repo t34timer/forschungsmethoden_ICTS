cb9982a622361aa85bb9359ef2981b2be0f3981c
Benedikt Kersjes <mail@benedikt-kersjes.de>
2016-01-03 20:22:44 +0100

    Continued work on Matcher

--- src/src/com/uni/stuttgart/fswt/icts/Controller/Matcher.java
+++ src/src/com/uni/stuttgart/fswt/icts/Controller/Matcher.java
@@ -14,15 +14,46 @@ public class Matcher {
 
     public static void match(ArrayList<Issue> issues, ArrayList<Commit> commits) {
         for (Commit commit : commits) {
+            Issue bestIssue = null;
+            int bestScore = 0;
+
             for (Issue issue : issues) {
+                int[] bestMatches = new int[5];
+
                 for (String commitWord : commit.getCommentTokens().getTokens().keySet()) {
                     for (String issueWord : issue.getDescriptionTokens().getTokens().keySet()) {
                         if (commitWord.equals(issueWord)) {
                             int wordCount = TokenManager.getWordCount(issueWord);
-                            System.out.println(issueWord + " " + wordCount + " " + Matcher.calculateWeight(wordCount));
+                            int weight = Matcher.calculateWeight(wordCount);
+
+                            for (int j = 0; j < bestMatches.length; j++) {
+                                if (weight > bestMatches[j]) {
+                                    System.arraycopy(bestMatches, j, bestMatches, j + 1, bestMatches.length - j - 1);
+                                    bestMatches[j] = weight;
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
+
+                int matchScore = 0;
+
+                for (int i : bestMatches) {
+                    matchScore += i;
+                }
+
+                if (matchScore > bestScore) {
+                    bestIssue = issue;
+                    bestScore = matchScore;
+                }
+            }
+
+            if (bestScore > 0) {
+                System.out.println("Matching commit " + commit.getId() + " with issue " + bestIssue.getId()
+                + " (score: " + bestScore + ")");
+            } else {
+                System.out.println("No Match found for " + commit.getId());
             }
         }
     }
